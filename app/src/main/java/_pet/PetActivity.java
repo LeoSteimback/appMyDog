63d12bbc253ef3f4ec2b6a2d1fafce3e1cc72d64
leonardo <leotosko@terra.com.br>
2019-10-10 11:18:12 -0300

    Criando o codigo para tirar foto do pet, mais ainda falta salvar no banco

--- app/src/main/java/_pet/PetActivity.java
+++ app/src/main/java/_pet/PetActivity.java
@@ -1,9 +1,14 @@
 package _pet;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -11,6 +16,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,11 +27,39 @@ public class PetActivity extends AppCompatActivity {
 
     final int REQUEST_CAMERA = 100;
     final PetsDB pdb = new PetsDB(this,"db_dogvacina.db",1);
+    ImageView imageViewFoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pets);
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 0);
+        }
+
+        imageViewFoto = (ImageView) findViewById(R.id.imgFoto);
+        findViewById(R.id.btnFoto).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tirarFoto();
+            }
+        });
+    }
+
+    public  void tirarFoto(){
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imagem = (Bitmap)extras.get("data");
+            imageViewFoto.setImageBitmap(imagem);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     public void camera(View v){
